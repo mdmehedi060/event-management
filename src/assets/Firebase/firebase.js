6706293d335964847b926e1f91d97572ed7e56e6
mdmehedi060 <mdmehedi060@gmail.com>
2023-10-08 23:24:08 +0600

    updated firebase config

--- src/assets/Firebase/firebase.js
+++ src/assets/Firebase/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBiodR72d6khYfv_OEqM_1MRjoegofuTBE",
-  authDomain: "assigntment-nine.firebaseapp.com",
-  projectId: "assigntment-nine",
-  storageBucket: "assigntment-nine.appspot.com",
-  messagingSenderId: "487523130280",
-  appId: "1:487523130280:web:b827d577912b0dfb6a5b9d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
